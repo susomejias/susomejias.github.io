cdc436eaf384f0d7847501533bda67648d380dd7
susomejias <jesusmejias.jm@gmail.com>
2018-10-29 08:20:41 +0100

    add changes

--- ejercicios/tema3/Calculadora-MejiasLeivaJesus/js/main.js
+++ ejercicios/tema3/Calculadora-MejiasLeivaJesus/js/main.js
@@ -6,12 +6,22 @@
 {
 
     let main;
+    let ce;
+    let btn7;
+    let buttons;
+    let output;
+    let calculadora;
 
     function init(){
 
         main = document.getElementsByTagName("main")[0];
 
+
         createLayout();
+
+        functionality();
+
+
 
     }
 
@@ -19,6 +29,7 @@
         Función para crear el layout de la calculadora.
     */ 
     let createLayout = function () { 
+
 
         let fragment = document.createDocumentFragment();
 
@@ -34,7 +45,10 @@
         // crear input para mostrar la salida
         let showInput = document.createElement("input");
         showInput.type = "text";
-        showInput.value = 0;
+        showInput.value = "0";
+        let attrId = document.createAttribute("id");
+        attrId.value = "output";
+        showInput.setAttributeNode(attrId);
         showContainerDiv.appendChild(showInput);
 
         // crear container-botones
@@ -46,17 +60,96 @@
 
         // array con el texto que contendrán los botones, ordenado para recorrerlo
         let orderText = ["CE","DEL","%","+","7","8","9","-","4","5","6","x","1","2","3","/","0","+/-",",","="];
+        
+        let orderIds = ["CE","DEL","percentage","sum","7","8","9","subtraction","4","5","6","multiplication","1","2","3","division","0","moreLess","coma","same"];
 
 
-        for (let i = 0; i < orderClasss.length; i++) {
+        orderClasss.forEach((element, index) => {
 
-            // crea cada boton con su clase y su contenido
-            createButton(buttonsContainerDiv, orderClasss[i], orderText[i]);
+            createButton(buttonsContainerDiv, orderClasss[index], orderText[index], orderIds[index]);
 
-        }
+        });
+
 
         main.appendChild(fragment);
 
+        output = document.getElementById("output");
+        buttons = document.getElementsByTagName("button");
+
+    }
+
+    let functionality = function (){
+
+
+
+        Array.from(buttons).forEach((element) => {
+
+
+            let idValue = element.getAttribute("id");
+
+            if (parseInt(idValue)) {
+
+                //console.log(idValue);
+
+                document.getElementById(idValue).addEventListener("click", () => { 
+                    output.value = parseInt(idValue) 
+                });
+                
+
+            }else{
+
+                //console.log(idValue);
+
+                let btn = document.getElementById(idValue);
+                if(btn){
+
+                    btn.addEventListener("click", () => {
+
+                        switch (idValue) {
+                            case "sum":
+                                    console.log("suma");
+                                break;
+                            case "multiplication":
+                                    console.log("multiplicacion");
+                                break;
+                            case "division":
+                                    console.log("division");
+                                break;
+                            case "moreLess":
+                                    console.log("masMenos");
+                                break;
+                            case "subtraction":
+                                    console.log("resta");
+                                break;
+                            case "same":
+                                    console.log("igual");
+                                break;
+                            case "percentage":
+                                    console.log("porcentaje");
+                                break;
+                            case "DEL":
+                                    console.log("borrar");
+                                break;
+                            case "CE":
+                                    console.log("resetear");
+                                break;
+                            case "coma":
+                                    console.log("coma");
+                                break;
+                            default:
+                                
+                        }
+
+                    });
+
+                }
+                
+
+
+
+            }
+
+        });
     }
 
     /**
@@ -66,11 +159,14 @@
         @param class clase para añadirle estilo al botón.
         @param text texto que se añadirá al botón.
     */ 
-    let createButton = function (parent, classs, text) {
+    let createButton = function (parent, classs, text, id) {
 
         let boton = document.createElement("button");
         boton.className = classs; 
         boton.textContent = text;
+        let attrId = document.createAttribute("id");
+        attrId.value = id;
+        boton.setAttributeNode(attrId);
         parent.appendChild(boton);
 
     }
