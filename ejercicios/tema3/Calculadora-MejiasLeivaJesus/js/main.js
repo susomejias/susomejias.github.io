c86d3faee0b9c099df013103a15e0eaa71f39529
susomejias <jesusmejias.jm@gmail.com>
2018-10-29 20:30:48 +0100

    add changes

--- ejercicios/tema3/Calculadora-MejiasLeivaJesus/js/main.js
+++ ejercicios/tema3/Calculadora-MejiasLeivaJesus/js/main.js
@@ -8,195 +8,225 @@
     let main;
     let buttons;
     let output;
-    let calculadora;
 
     function init(){
 
         main = document.getElementsByTagName("main")[0];
 
 
-        createLayout();
-
-        functionality();
-
+        calculadora.crateLayout();
+        calculadora.functionality();
 
 
     }
 
-    /**
-        Función para crear el layout de la calculadora.
-    */ 
-    let createLayout = function () { 
+    let calculadora = {
+
+        cumulative : 0,
+
+        flag: false,
+
+        operation: "",
+
+        crateLayout: function () { 
 
 
-        let fragment = document.createDocumentFragment();
+            let fragment = document.createDocumentFragment();
+    
+    
+            // crear container
+            let containerDiv = document.createElement("div");
+            this.addDiv(containerDiv, fragment , "container");
+    
+            // crear container-mostrar
+            let showContainerDiv = document.createElement("div");
+            this.addDiv(showContainerDiv, containerDiv, "show-container");
+    
+            // crear input para mostrar la salida
+            let showInput = document.createElement("input");
+            showInput.type = "text";
+            showInput.value = "0";
+            let attrId = document.createAttribute("id");
+            attrId.value = "output";
+            let disabled = document.createAttribute("disabled");
+            disabled.value = "disabled";
+            showInput.setAttributeNode(disabled);
+            showInput.setAttributeNode(attrId);
+            showContainerDiv.appendChild(showInput);
+    
+            // crear container-botones
+            let buttonsContainerDiv = document.createElement("div");
+            this.addDiv(buttonsContainerDiv, containerDiv, "buttons-container");
+    
+            // array con las clases ordenadas para recorrerlas
+            let orderClasss = ["btnRed","btnOrange","btnOperation","btnOperation","btnPrincipal", "btnPrincipal","btnPrincipal","btnOperation","btnPrincipal","btnPrincipal", "btnPrincipal","btnOperation","btnPrincipal","btnPrincipal","btnPrincipal", "btnOperation","btnPrincipal","btnOperation","btnGrey","btnAccent"];
+    
+            // array con el texto que contendrán los botones, ordenado para recorrerlo
+            let orderText = ["CE","DEL","%","+","7","8","9","-","4","5","6","x","1","2","3","/","0","+/-",",","="];
+            
+            let orderIds = ["CE","DEL","percentage","sum","7","8","9","subtraction","4","5","6","multiplication","1","2","3","division","0","moreLess","coma","same"];
+    
+    
+            orderClasss.forEach((element, index) => {
+    
+                this.createButton(buttonsContainerDiv, orderClasss[index], orderText[index], orderIds[index]);
+    
+            });
+    
+    
+            main.appendChild(fragment);
+    
+            output = document.getElementById("output");
+            buttons = document.getElementsByTagName("button");
+    
+        },
 
 
-        // crear container
-        let containerDiv = document.createElement("div");
-        addDiv(containerDiv, fragment , "container");
-
-        // crear container-mostrar
-        let showContainerDiv = document.createElement("div");
-        addDiv(showContainerDiv, containerDiv, "show-container");
-
-        // crear input para mostrar la salida
-        let showInput = document.createElement("input");
-        showInput.type = "text";
-        showInput.value = "0";
-        let attrId = document.createAttribute("id");
-        attrId.value = "output";
-        showInput.setAttributeNode(attrId);
-        showContainerDiv.appendChild(showInput);
-
-        // crear container-botones
-        let buttonsContainerDiv = document.createElement("div");
-        addDiv(buttonsContainerDiv, containerDiv, "buttons-container");
-
-        // array con las clases ordenadas para recorrerlas
-        let orderClasss = ["btnRed","btnOrange","btnOperation","btnOperation","btnPrincipal", "btnPrincipal","btnPrincipal","btnOperation","btnPrincipal","btnPrincipal", "btnPrincipal","btnOperation","btnPrincipal","btnPrincipal","btnPrincipal", "btnOperation","btnPrincipal","btnOperation","btnGrey","btnAccent"];
-
-        // array con el texto que contendrán los botones, ordenado para recorrerlo
-        let orderText = ["CE","DEL","%","+","7","8","9","-","4","5","6","x","1","2","3","/","0","+/-",",","="];
-        
-        let orderIds = ["CE","DEL","percentage","sum","7","8","9","subtraction","4","5","6","multiplication","1","2","3","division","0","moreLess","coma","same"];
+        functionality: function (){
 
 
-        orderClasss.forEach((element, index) => {
-
-            createButton(buttonsContainerDiv, orderClasss[index], orderText[index], orderIds[index]);
-
-        });
-
-
-        main.appendChild(fragment);
-
-        output = document.getElementById("output");
-        buttons = document.getElementsByTagName("button");
-
-    }
-
-    let functionality = function (){
-
-
-
-        Array.from(buttons).forEach((element) => {
-
-
-            let idValue = element.getAttribute("id");
-
-            if (parseInt(idValue)) {
-
-                //console.log(idValue);
-
-                document.getElementById(idValue).addEventListener("click", () => { 
-                    
-                    
-                    if(output.value === "0"){
-                        output.value = idValue;
-                    }else {
-                        output.value += idValue;
-                    }
-                });
-                
-
-            }else{
-
-                //console.log(idValue);
-
-                let btn = document.getElementById(idValue);
-                if(btn){
-
-                    btn.addEventListener("click", () => {
-
-                        switch (idValue) {
-                            case "sum":
-                                    console.log("suma");
-                                break;
-                            case "multiplication":
-                                    console.log("multiplicacion");
-                                break;
-                            case "division":
-                                    console.log("division");
-                                break;
-                            case "moreLess":
-                                    console.log("masMenos");
-                                break;
-                            case "subtraction":
-                                    console.log("resta");
-                                break;
-                            case "same":
-                                    console.log("igual");
-                                break;
-                            case "percentage":
-                                    console.log("porcentaje");
-                                break;
-                            case "DEL":
-                                    let deleteString = output.value.substring(0, output.value.length -1);
-                                    console.log(deleteString);
-                                    output.value = deleteString;
-                                    if ( output.value === "") {
-                                        output.value = 0;
-                                    }
-                                    //console.log("borrar");
-                                break;
-                            case "CE":
-                                    output.value = 0;
-                                    //console.log("resetear");
-                                break;
-                            case "coma":
-                                    if (!output.value.includes(",")) {
-                                        output.value += ",";
-                                    }
-                                    //console.log("coma");
-                                break;
-                            default:
-                                
+            Array.from(buttons).forEach((element) => {
+    
+    
+                let idValue = element.getAttribute("id");
+    
+                if (parseFloat(idValue)) {
+    
+                    //console.log(idValue);
+    
+                    document.getElementById(idValue).addEventListener("click", () => { 
+                        
+                        
+                        if(output.value === "0" || flag){
+                            output.value = parseFloat(idValue);
+                            flag = false
+                        }else {
+                            output.value += parseFloat(idValue);
                         }
-
+    
                     });
-
+                    
+    
+                }else{
+    
+                    //console.log(idValue);
+    
+                    let btn = document.getElementById(idValue);
+                    if(btn){
+    
+                        btn.addEventListener("click", () => {
+    
+                            switch (idValue) {
+                                case "sum":
+                                        cumulative += parseFloat(output.value);
+                                        flag = true;
+                                        operation = "sum";
+                                        //output.value = cumulative;
+                                        //console.log(cumulative);
+                                    break;
+                                case "multiplication":
+                                        console.log("multiplicacion");
+                                    break;
+                                case "division":
+                                        console.log("division");
+                                    break;
+                                case "moreLess":
+    
+                                        if (parseFloat(output.value) > 0 ) {
+                                            output.value = (parseFloat(output.value) * -1).toString();
+                                        }else {
+                                            //console.log(output.value); 
+                                            output.value = output.value.replace("-" , "");
+                                        }
+                                    break;
+                                case "subtraction":
+                                        if (!output.value.includes("-")){
+                                            output.value = "-";
+                                        }
+                                        //console.log("resta");
+                                    break;
+                                case "same":
+    
+                                        if (operation === "sum"){
+                                            cumulative += parseFloat(output.value);
+                                            output.value = cumulative;
+                                        }
+    
+                                        flag = false;
+                                        console.log(cumulative);
+                                    break;
+                                case "percentage":
+                                        console.log("porcentaje");
+                                    break;
+                                case "DEL":
+                                        if (output.value.includes("-") && output.value.length === 2){
+                                            output.value = output.value.substring(1,2);
+                                        }
+                                        
+                                        output.value = output.value.slice(0, output.value.length -1);
+                                        
+    
+                                        if ( output.value === "") {
+                                            output.value = 0;
+                                            cumulative = 0;
+                                        }
+                                        flag = false;
+    
+                                        
+                                        //console.log("borrar");
+                                    break;
+                                case "CE":
+                                        output.value = 0;
+                                        cumulative = 0;
+                                        flag = false;
+                                        //console.log("resetear");
+                                    break;
+                                case "coma":
+                                        if (!output.value.includes(".")) {
+                                            output.value += ".";
+                                        }
+                                        //console.log("coma");
+                                    break;
+                                case "0":
+                                        if (output.value !== "0") {
+                                            output.value += "0"; 
+                                        }
+    
+                                    break;
+                                default:
+                                    
+                            }
+    
+                        });
+    
+                    }
+                    
+    
+    
+    
                 }
-                
+    
+            });
+        },
 
+        createButton : function (parent, classs, text, id) {
 
+            let boton = document.createElement("button");
+            boton.className = classs; 
+            boton.textContent = text;
+            let attrId = document.createAttribute("id");
+            attrId.value = id;
+            boton.setAttributeNode(attrId);
+            parent.appendChild(boton);
+    
+        },
 
-            }
+        addDiv: function (div, parent, classs) {
 
-        });
-    }
-
-    /**
-        Función para crear un botón.
-        
-        @param parent elemento padre al cual se añadirá el botón.
-        @param class clase para añadirle estilo al botón.
-        @param text texto que se añadirá al botón.
-    */ 
-    let createButton = function (parent, classs, text, id) {
-
-        let boton = document.createElement("button");
-        boton.className = classs; 
-        boton.textContent = text;
-        let attrId = document.createAttribute("id");
-        attrId.value = id;
-        boton.setAttributeNode(attrId);
-        parent.appendChild(boton);
-
-    }
-
-
-    /**
-        Función para crear un botón.
-
-        @param div elemento div que se añadirá al padre y al que se le añadirán los estilos.
-        @param parent elemento padre al que se añadira el elemento div.
-        @param class clase para añadirle estilos al div.
-    */ 
-    let addDiv = function (div, parent, classs) {
-
-        div.className = classs; 
-        parent.appendChild(div);
+            div.className = classs; 
+            parent.appendChild(div);
+    
+        }
 
     }
 
