515395b0d86fb41cb040a7855838b80892ac89b6
susomejias <jesusmejias.jm@gmail.com>
2018-11-12 09:20:39 +0100

    fix, list loop collections

---
+++ ejercicios/tema3/prueba/main.js
@@ -0,0 +1,5 @@
+var validator = require("validator");
+//const validator = require("validator");
+let email = "jesus.com";
+let email1 = "jesusmejias.jm@gmail.com";
+console.log(validator.isEmail(email1));
