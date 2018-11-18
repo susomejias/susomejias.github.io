3b4f31a2a08a60b53532ee1a889e991c993a2950
susomejias <jesusmejias.jm@gmail.com>
2018-11-18 14:28:03 +0100

    fixed regex and destructuring params

--- ejercicios/tema3/ExamenJs-MejiasLeivaJesus/js/ejercicio2.js
+++ ejercicios/tema3/ExamenJs-MejiasLeivaJesus/js/ejercicio2.js
@@ -30,32 +30,10 @@
     this.name = name;
   };
 
-  let validateNombreApellidos = function() {
-    let regex = /(\s?[a-zA-z]+\s?) (\s?[a-zA-z]+\s?)+,(\s?[a-zA-z]+\s?)$/g;
+  let validarSetCollection = function(rnombre, apellido1, apellido2) {
+    console.log(arguments);
 
-    let inputValue = input.value;
-
-    let values = regex.exec(inputValue);
-
-    try {
-      console.log(regex.exec(inputValue));
-      if (regex.test(inputValue)) {
-        if (values !== null) {
-          spanError.textContent = "";
-          [, apellido1, apellido2, rnombre] = values;
-          nombre.textContent = `Nombre:  ${rnombre}`;
-          apellidos.textContent = `Apellido:  ${apellido1 + ", " + apellido2}`;
-        }
-      } else {
-        let miErrorException = new ErrorException(
-          "Error. Formato correcto: Cuadrado Perfecto, Anacleto",
-          "error"
-        );
-        throw miErrorException;
-      }
-
-      // ejercicio 3
-
+    if (arguments.length === 2) {
       if (!setValidate.has(rnombre.trim())) {
         setValidate.add(rnombre.trim());
       } else {
@@ -67,16 +45,68 @@
       } else {
         spanError.textContent += " (primer apellido repetido) ";
       }
+    } else if (arguments.length === 3) {
+      if (!setValidate.has(rnombre.trim())) {
+        setValidate.add(rnombre.trim());
+      } else {
+        spanError.textContent += " (nombre repetido) ";
+      }
 
+      if (!setValidate.has(apellido1.trim())) {
+        setValidate.add(apellido1.trim());
+      } else {
+        spanError.textContent += " (primer apellido repetido) ";
+      }
       if (!setValidate.has(apellido2.trim())) {
         setValidate.add(apellido2.trim());
       } else {
         spanError.textContent += " (segundo apellido repetido) ";
       }
+    }
+  };
+  let validateNombreApellidos = function() {
+    let regex = /(\s?[a-zA-ZáéíóúÁÉÍÓÚñÑ]+\s?)?(\s?[a-zA-ZáéíóúÁÉÍÓÚñÑ]+\s?)?,(\s?[a-zA-záéíóúÁÉÍÓÚñÑ]+\s?)$/g;
+    //let regex = /(\s?[a-zA-ZáéíóúÁÉÍÓÚñÑ]+\s?) (\s?[a-zA-ZáéíóúÁÉÍÓÚñÑ]+\s?)+,(\s?[a-zA-ZáéíóúÁÉÍÓÚñÑ]+\s?)$/g;
+
+    let inputValue = input.value;
+    //console.log(regex.exec(inputValue));
+
+    let values = regex.exec(inputValue);
+
+    console.log(values);
+
+    try {
+      if (values !== null) {
+        if (values[2] === undefined) {
+          spanError.textContent = "";
+          [, apellido1, , rnombre] = values;
+          nombre.textContent = `Nombre:  ${rnombre}`;
+          apellidos.textContent = `Apellido:  ${apellido1}`;
+          let arg = [rnombre, apellido1];
+          validarSetCollection(...arg);
+        } else {
+          spanError.textContent = "";
+          [, apellido1, apellido2, rnombre] = values;
+          nombre.textContent = `Nombre:  ${rnombre}`;
+          apellidos.textContent = `Apellido:  ${apellido1 + ", " + apellido2}`;
+          let arg = [rnombre, apellido1, apellido2];
+          validarSetCollection(...arg);
+        }
+      } else {
+        let miErrorException = new ErrorException(
+          "Error. Formato correcto: Cuadrado Perfecto, Anacleto",
+          "error"
+        );
+        throw miErrorException;
+      }
+
+      // ejercicio 3
 
       //console.log(setValidate);
     } catch (e) {
-      spanError.textContent = e.message;
+      if (e instanceof ErrorException) {
+        spanError.textContent = e.message;
+      }
     }
   };
   window.addEventListener("load", init);
