3e954724401b0c44cabb46933b03212f77e4f8d0
susomejias <jesusmejias.jm@gmail.com>
2019-01-14 20:34:05 +0100

    fix alert close #4

--- ejercicios/tema7/Códigos-manual-jQuery.ParteI-II-III-IV-V-VI-JesúsMejiasLeiva/parteIII/demo1.js
+++ ejercicios/tema7/Códigos-manual-jQuery.ParteI-II-III-IV-V-VI-JesúsMejiasLeiva/parteIII/demo1.js
@@ -5,9 +5,13 @@
 function init() {
   $("#boton").click(function() {
     let selectorEscrito = $("#camposelector").val();
+    let $msg = $("#msg");
     if (selectorEscrito == "") {
-      alert("Escribe algo en el campo de texto");
+      
+      $msg.text("Escribe algo en el campo de texto");
+      $msg.css("color", "red");
     } else {
+      $msg.text("");
       $(selectorEscrito).fadeOut("slow", function() {
         $(selectorEscrito).fadeIn("slow");
       });
