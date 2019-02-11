0f4f5348e36a3dda3992faf478da4ca34ab788ba
susomejias <jesusmejias.jm@gmail.com>
2019-02-11 18:46:00 +0100

    add exercise Ajax con Jquery - Cargando archivos del servidor.

---
+++ ejercicios/tema7/Ajax/AJAX-con-jQuery.Cargando-ficheros-del-servidor/js/main.js
@@ -0,0 +1,51 @@
+/*
+
+Página html con una caja de texto donde se indica una url que vas a descargar del servidor. Inicialmente aparecerá la propia página
+
+Al pulsar el botón "mostrar" mostrará el fichero en un scroller
+
+En un div debe mostrarse en todo momento el estado en el que se encuentra la petición AJAX, indicando tanto el valor numérico como su descripción ("No inicializada", "Abierta"... "Completada") (readyState)
+
+También se mostrará el código y texto del estado de la respuesta del servidor en otro div (status y statusText)
+
+Utiliza estilos y diseña la página de forma equilibrada.
+
+En la misma ruta, deja un fichero index2.html para probar la carga de otros ficheros
+
+@author Jesús Mejías Leiva
+
+*/
+
+{
+
+
+  let init = function(){
+
+    $("#btnShow").click(GetHtml);
+
+  }
+
+  let GetHtml = function(){
+
+      let actions = "";
+
+      $.ajax({
+        method: "GET",
+        url: "index2.html",
+        beforeSend: ()=> actions += "<p>beforeSend</p>",
+        complete: ()=> {
+          actions += "<p>complete</p>";
+          $("#showState").html(actions);
+        },
+        success: (data)=> {
+          $("#showInfo").text(data);
+          actions += "<p>success</p>";
+        },
+        error: ()=> actions += "<p>error</p>"
+      });
+
+  }
+
+  $(init)
+
+}
