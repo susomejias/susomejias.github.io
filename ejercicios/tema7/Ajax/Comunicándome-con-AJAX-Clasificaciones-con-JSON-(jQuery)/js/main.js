43512ae708eb27abb6776a9795dacbccb1b6d3eb
susomejias <jesusmejias.jm@gmail.com>
2019-02-11 20:17:39 +0100

    add exercise Comunicándome con AJAX: Clasificaciones con JSON (jQuery)

---
+++ ejercicios/tema7/Ajax/Comunicándome-con-AJAX-Clasificaciones-con-JSON-(jQuery)/js/main.js
@@ -0,0 +1,50 @@
+/*
+Para ello implementa una página con tres botontones donde descargues y muestres, según el botón:
+
+Habilidades para la vida y una breve descripción.
+
+Perfiles IT más demandados y una breve descripción.
+
+Tipos de desarrolladores web y una lista de habilidades.
+
+@author Jesús Mejías Leiva
+*/
+{
+
+  let init = function(){
+
+      $("button").click(function(){
+        showJSON("./JSON/" + $(this).attr("jsonFile"));
+      });
+
+
+
+  }
+
+  let showJSON = function(url){
+     $.getJSON(url,function(data) {
+       let html = ``;
+       $.each(data, function(index,value) {
+
+          if(value.skill && value.description){
+              html+= `<p><b>${value.skill}</b>: ${value.description}</p>`
+          }else if (value.profile && value.description){
+            html+= `<p><b>${value.profile}</b>: ${value.description}</p>`
+          }else if (value.type && value.skills){
+            html+= `<p><b>${value.type}</b>:</p>`;
+            for (let skill of value.skills) {
+              //console.log(skill);
+              html+= `<p>${skill}</p>`
+            }
+
+          }
+
+        });
+       $("#showInfo").html(html)
+    });
+
+  }
+
+  $(init);
+
+}
