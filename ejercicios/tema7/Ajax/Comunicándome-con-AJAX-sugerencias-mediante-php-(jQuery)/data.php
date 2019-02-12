c1118dc693c345645914ac0c18e8bec9156af4d1
susomejias <jesusmejias.jm@gmail.com>
2019-02-12 18:42:23 +0100

    add info data.php

--- ejercicios/tema7/Ajax/Comunicándome-con-AJAX-sugerencias-mediante-php-(jQuery)/data.php
+++ ejercicios/tema7/Ajax/Comunicándome-con-AJAX-sugerencias-mediante-php-(jQuery)/data.php
@@ -4,10 +4,18 @@ header('Content-type: text/plain; charset=utf-8');
 
 $students = array(
     array('first_name' => 'Jesús', 'last_name' => 'Mejías Leiva'),
-    array('first_name' => 'Javier', 'last_name' => 'perez perez'),
+    array('first_name' => 'José Rafael', 'last_name' => 'Barbara'),
     array('first_name' => 'Guillermo', 'last_name' => 'Boquizo Sánchez'),
     array('first_name' => 'Angelo', 'last_name' => 'Barbara'),
-    array('first_name' => 'Rafael', 'last_name' => 'Garcia Zurita')
+    array('first_name' => 'José Manuel', 'last_name' => 'Bravo Martínez'),
+    array('first_name' => 'Juan Antonio', 'last_name' => 'Bujalance Martínez'),
+    array('first_name' => 'Javier', 'last_name' => 'Frías Serrano'),
+    array('first_name' => 'Samuel', 'last_name' => 'Luque Reyes'),
+    array('first_name' => 'Adrián', 'last_name' => 'Moya Moruno'),
+    array('first_name' => 'Mario', 'last_name' => 'Navarro Madrid'),
+    array('first_name' => 'Francisco', 'last_name' => 'Ramírez Ruíz'),
+    array('first_name' => 'José María', 'last_name' => 'Romero Ruíz'),
+    array('first_name' => 'Rafael', 'last_name' => 'Sojo Ruíz'),
 );
 $results = array();
 
