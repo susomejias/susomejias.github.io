215b50a87ccfa3848cf5c040c936095694a45381
susomejias <jesusmejias.jm@gmail.com>
2019-02-18 13:15:16 +0100

    refactor clasificación con JSON exercise

--- ejercicios/tema7/Ajax/Comunicándome-con-AJAX-Clasificaciones-con-JSON-(jQuery)/js/main.js
+++ ejercicios/tema7/Ajax/Comunicándome-con-AJAX-Clasificaciones-con-JSON-(jQuery)/js/main.js
@@ -27,24 +27,71 @@ Tipos de desarrolladores web y una lista de habilidades.
 
   let showJSON = function(url){
      $.getJSON(url,function(data) {
-       let html = ``;
+
+       let selectCode = `<select name="" id="selectCode" >`;
+       let devType = ``;
+
        $.each(data, function(index,value) {
 
           if(value.skill && value.description){
-              html+= `<p><b>${value.skill}</b>: ${value.description}</p>`
+            selectCode += `
+                <option value="${value.description}">${value.skill}</option>
+            `;
+              //html+= `<p><b>${value.skill}</b>: ${value.description}</p>`
           }else if (value.profile && value.description){
-            html+= `<p><b>${value.profile}</b>: ${value.description}</p>`
+            selectCode += `
+                <option value="${value.description}">${value.profile}</option>
+            `;
+            //selectCode+= `<p><b>${value.profile}</b>: ${value.description}</p>`
           }else if (value.type && value.skills){
-            html+= `<p><b>${value.type}</b>:</p>`;
+
+            devType += `
+                    <div id="container">
+                    <b class="ripple">${value.type}</b>
+                    <div class="skills">
+                    `;
             for (let skill of value.skills) {
-              //console.log(skill);
-              html+= `<p>${skill}</p>`
+              devType += `
+                  <p>${skill}</p>
+              `
             }
 
+            devType += `</div>`;
           }
 
         });
-       $("#showInfo").html(html)
+
+        // skill life
+        selectCode += `
+              </select>
+              <p id='show'></p>
+        `;
+
+        $("#showInfo").html(selectCode)
+
+        $("#show").fadeIn("slow").html($("#selectCode").val());
+
+        $("#selectCode").change(function(event) {
+            $("#show").fadeIn("slow").html($(this).val());
+        });
+
+
+        // CHECKS DEVS
+
+        if (devType !== ''){
+          devType += `</div>`;
+            $("#showInfo").html(devType);
+            $(".skills").css("display", "none");
+            $("#container b").click(function(event) {
+                let parentElement = $(this).parent();
+                let div = parentElement.children(".skills")
+                div.toggle("slow");
+            });
+        }
+
+
+
+
     });
 
   }
